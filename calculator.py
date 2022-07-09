14e36d9a9c987ca70468e219857ab2971679acd2
Anton Ivanov <anton260601@gmail.com>
2022-07-09 21:58:03 +0300

    DS_Store - new file, calculator - new file

---
+++ calculator.py
@@ -0,0 +1,31 @@
+
+action = input("Введите операцию: ")
+amount = int(input("Сколько операндов? "))
+for order in range(1, amount + 1):
+    number = int(input("Введите операнд", str(order) + ":"))
+
+
+
+
+
+
+
+
+
+
+if action == "X" or action == "*":
+    print(A, action, B, "=", A * B)
+elif action == "/":
+    print(A, action, B, "=", A / B)
+elif action == "+":
+    print(A, action, B, "=", A + B)
+elif action == "-":
+    print(A, action, B, "=", A - B)
+
+while action != "X" and action != "*" and action != "/" and action != "+" and action != "-":
+    print("Ошибка! Повторите ввод!")
+    action = input("Введите операцию: ")
+    A = int(input("Введите первое число: "))
+    B = int(input("Введите второе число: "))
+
+
